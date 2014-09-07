6169afcc3d2808df07d7bd102963de8b0e57f6f1
Jim Grant <jimgrant@gmail.com>
2014-09-07 16:32:49 -0700

    first commit

---
+++ client/js/client.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var $ = require('jquery'),
+    _ = require('lodash'),
+    Backbone = require('backbone');
+    Backbone.$ = $;
+
+//executed on document ready
+$(function () {
+  $('#content').html('<h1>hi there</h1>');
+});
